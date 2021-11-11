bbc1ae70fe49fc025128a996e6e8477e9fa9cdae
Zaliro <zalir0@live.fr>
2021-11-11 18:14:41 +0100

    useSystemColorMode

--- src/layout/Theme.jsx
+++ src/layout/Theme.jsx
@@ -1,6 +1,10 @@
 import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
+  config: {
+    initialColorMode: 'light',
+    useSystemColorMode: false,
+  },
   colors: {
     brand: {
       100: '#cef6e8',
